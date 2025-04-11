b67242783ff0e3bb9b25ffaf36d6c913814a1106
Wssena <senawesley046@gmail.com>
2025-04-10 22:35:46 -0300

    Create calculadora .sh

---
+++ calculadora.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Iniciando o Scipt "
+
+echo "Calculadora programada"
+
+# Quando solicitado digite o sinal matemático da operação.
+# Atente-se para a ortografia correta!!
+echo' print (Quando solicitado digite o sinal matemático da operação. ex: + ou *) '
+echo' while True '
+ echo' print (Para proseguirmos com a calculadora inteligente faremos algumas perguntas) '
+ ' nome= input (Qual seu nome ?) '
+ ' city= input (Qual é o nome da sua cidade ) '
+ ' state= (Qual o nome do seu estado ? ) '
+ ' idade= input (Quantos anos você tem ?) '
+' gen= input (Qual gênero te define, Masculino ou Feminino ?) '
+' if gen == feminino: '
+ ' print (Seja bem vinda !) '
+ ' else: '
+ ' print (Seja bem vindo !) '
+ ' n1= float(input (Digite um número )) '
+ ' n2= float(input (Digite outro número )) '
+' n3= (input (Qual função matemática deseja? )) '
+' if n3 == *: '
+ ' print (Resultado,n1*n2) '
+' elif n3 == +:'
+ ' print (Resultado,n1+n2) '
+ ' if n3 == -:'
+ ' print (Resultado,n1-n2) '
+' elif n3 == /:'
+ '  print (Resultado,n1/n2) '
+ ' exit= input (Deseja continuar no programa Sim/Não?) '
+' if exit == sim: '
+ ' print (Reiniciando o programa ) '
+' else: '
+ ' print (Obrigado por usar o programa, até mais!!! ) '
+ ' break'
